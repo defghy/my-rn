300b50f1007e5dae35399da67288aad1f7d5097f
huyu <huyu@meicai.cn>
2019-05-04 17:36:01 +0800

    动画测试

--- app/Component/svg.js
+++ app/Component/svg.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Animated } from 'react-native';
 import Svg,{
   Circle,
   Ellipse,
@@ -23,23 +24,68 @@ import Svg,{
   Mask,
 } from 'react-native-svg';
 
+const AnimatedPath = Animated.createAnimatedComponent(Path);
+const ani = new Animated.Value(0);
+
 class SvgTest extends React.Component {
+  state = {
+    len: new Animated.Value(0),
+    fade: new Animated.Value(0),
+    animatedFill: new Animated.Value(0),
+    path: ani.interpolate({
+      inputRange: [0, 1],
+      outputRange: [`M0,0L0,0`, `M0,0L400,400`],
+    })
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      const { len } = this.state;
+      if (len<=400) {
+        this.setState({
+          len: len + 20
+        });
+      }
+    }, 1000);  
+    const fadeAni = Animated.timing(                  // 随时间变化而执行动画
+      this.state.fade,            // 动画中的变量值
+      {
+        toValue: 1,                   // 透明度最终变为1，即完全不透明
+        duration: 500,              // 让动画持续一段时间
+        useNativeDriver: true
+      }
+    );
+
+    const lenAni = Animated.timing(                  // 随时间变化而执行动画
+      ani,            // 动画中的变量值
+      {
+        toValue: 1,                   // 透明度最终变为1，即完全不透明
+        duration: 3000
+      }
+    );  
+
+    Animated.parallel([fadeAni, lenAni]).start();
+  }
   render() {
+    const { len } = this.state;
     return (
+      <Animated.View                 // 使用专门的可动画化的View组件
+        style={{
+          opacity: this.state.fade,         // 将透明度指定为动画变量值
+        }}
+      >
       <Svg
-          width="50%"
-          height="130"
+          width="300"
+          height="300"
           fill="#fff"
           stroke="#00f"
           color="#f00"
-          viewBox="-16 -16 544 544"
+          viewBox="0 0 400 400"
       >
-          <Path
-              d="M318.37,85.45L422.53,190.11,158.89,455,54.79,350.38ZM501.56,60.2L455.11,13.53a45.93,45.93,0,0,0-65.11,0L345.51,58.24,449.66,162.9l51.9-52.15A35.8,35.8,0,0,0,501.56,60.2ZM0.29,497.49a11.88,11.88,0,0,0,14.34,14.17l116.06-28.28L26.59,378.72Z"
-              strokeWidth="32" stroke="#00f"
-          />
-          <Path d="M0,0L512,512" stroke="#0f0" strokeWidth="32" />
+        <Circle cx="200" cy="200" r="200" fill="#fdd" stroke="none" />
+        <AnimatedPath d={this.state.path} stroke="#0f0" strokeWidth="32" />
       </Svg>
+      </Animated.View>
     );
   }
 }
--- app/detail.js
+++ app/detail.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, PanResponder } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 import Svg from './Component/svg.js';
 
 class Detail extends React.Component {
+
+  _panResponder = PanResponder.create({
+    onMoveShouldSetPanResponder: (evt, gestureState) => true,
+    onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+    onPanResponderMove: (evt, gestureState) => {
+      debugger;
+    },
+  });
+
+  componentDidMount() {
+  
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} {...this._panResponder.panHandlers} >
         <Text>详情页</Text>
-        <View style={{width: '100%', height: 300}}>
-          <Svg style={{width: '100%', height: '100%'}}/>
+        <View style={{width: '100%', height: 500}}>
+          <Svg />
         </View>
       </View>
     );
