c8326f9108f1f1fac3416a4eb651a9775091c0a1
huyu <huyu@meicai.cn>
2019-06-08 14:54:23 +0800

    fix

--- app/entry.js
+++ app/entry.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { createBottomTabNavigator, createAppContainer, createStackNavigator } from 'react-navigation';
-import webviewTest from './webview.js';
-import Test from './test.js';
 import Detail from './detail.js';
 import jokeList from './jokeList.js';
 import Icon from 'react-native-vector-icons/FontAwesome';
