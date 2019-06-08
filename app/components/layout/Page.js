efda379d046474b41fcf5e6c43d50517e5e32cf5
huyu <huyu@meicai.cn>
2019-06-08 14:04:03 +0800

    noth screen

--- app/components/layout/Page.js
+++ app/components/layout/Page.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { View, StatusBar } from 'react-native';
+import { View } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
+import { STATUS_BAR_HEIGHT } from 'MYRN/app/utils/global';
+
 class Page extends React.Component {
+  static defaultProps = {
+    statusBarBackgroundColor: '#fff' // 默认选中
+  };
+
   state = {
 
   };
@@ -13,20 +19,28 @@ class Page extends React.Component {
   }
 
   render() {
-    const { children } = this.props;
+    const { children, statusBarBackgroundColor } = this.props;
     return (
-      <SafeAreaView style={styles.page}>
-        {children}
+      <SafeAreaView>
+        <View style={[
+          styles.statusBar, {backgroundColor: statusBarBackgroundColor}
+        ]}></View>
+        <View style={styles.page}>
+          {children}
+        </View>
       </SafeAreaView>
     );
   }
 }
 
 const styles = EStyleSheet.create({
+  statusBar: {
+    height: STATUS_BAR_HEIGHT, width: '100%',
+    position: 'absolute', left: 0, top: 0
+  },
   page: {
     flexDirection: 'column', height: '100%',
     backgroundColor: '#f5f5f5',
-    paddingTop: StatusBar.currentHeight
   }
 });
 
---
+++ app/utils/global.js
@@ -0,0 +1,18 @@
+import { Dimensions, Platform, StatusBar } from 'react-native';
+
+const { height: DEVICE_HEIGHT, width: DEVICE_WIDTH } = Dimensions.get('window');
+
+export const IS_IPHONEX = (function() {
+  const iphones = {
+    'x': [375, 812],
+    'xsmax': [414, 896]
+  };
+  const [w, h] = [Math.min(DEVICE_WIDTH, DEVICE_HEIGHT), Math.max(DEVICE_WIDTH, DEVICE_HEIGHT)];
+  return Platform.OS === 'ios' &&
+    !Platform.isPad &&
+    !Platform.isTVOS &&
+    !!Object.values(iphones).find(([iw, ih]) => (iw === w && ih === h));
+})();
+
+export const STATUS_BAR_HEIGHT = Platform.OS === 'ios' ?
+  (IS_IPHONEX ? 44 : 20) : StatusBar.currentHeight;
