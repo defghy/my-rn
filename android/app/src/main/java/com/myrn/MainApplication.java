493b89e1b9e69d3938ae8015a0126ad089956d17
huyu <huyu@meicai.cn>
2019-06-22 15:40:14 +0800

    android兼容需要降级

--- android/app/src/main/java/com/myrn/MainApplication.java
+++ android/app/src/main/java/com/myrn/MainApplication.java
@@ -2,17 +2,19 @@ package com.myrn;
 
 import android.app.Application;
 
-import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import org.reactnative.camera.RNCameraPackage;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
-import com.horcrux.svg.SvgPackage;
+import org.reactnative.camera.RNCameraPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -25,12 +27,15 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      @SuppressWarnings("UnnecessaryLocalVariable")
-      List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
-      packages.add(new SvgPackage());
-      return packages;
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNCWebViewPackage(),
+            new VectorIconsPackage(),
+            new SvgPackage(),
+            new RNDeviceInfo(),
+            new RNCameraPackage(),
+            new RNGestureHandlerPackage()
+      );
     }
 
     @Override
