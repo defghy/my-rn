8ec03451bde5d0d366b283c337fd47784861bdac
huyu <huyu@meicai.cn>
2019-06-27 16:37:30 +0800

    测试snapshot

--- android/app/src/main/java/com/myrn/MainApplication.java
+++ android/app/src/main/java/com/myrn/MainApplication.java
@@ -3,6 +3,7 @@ package com.myrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNViewShotPackage(),
             new RNCWebViewPackage(),
             new VectorIconsPackage(),
             new SvgPackage(),
