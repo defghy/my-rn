c8818ae8638e9c90a09955e9d7b5f8988d2453f3
huyu <huyu@meicai.cn>
2019-08-18 20:10:54 +0800

    android native modules

--- android/app/src/main/java/com/myrn/MainApplication.java
+++ android/app/src/main/java/com/myrn/MainApplication.java
@@ -14,6 +14,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.myrn.rnbridge.HelloWorldPackage; // Add this import statement
 
 import java.util.Arrays;
 import java.util.List;
@@ -36,7 +37,8 @@ public class MainApplication extends Application implements ReactApplication {
             new SvgPackage(),
             new RNDeviceInfo(),
             new RNCameraPackage(),
-            new RNGestureHandlerPackage()
+            new RNGestureHandlerPackage(),
+            new HelloWorldPackage()
       );
     }
 
---
+++ android/app/src/main/java/com/myrn/rnbridge/HelloWorld.java
@@ -0,0 +1,24 @@
+// android/app/src/main/java/com/cppreactnative/helloworld/HelloWorldModule.java
+package com.myrn.rnbridge;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class HelloWorld extends ReactContextBaseJavaModule {
+
+    public HelloWorld(ReactApplicationContext reactContext) {
+        super(reactContext); //required by React Native
+    }
+
+    @Override
+    public String getName() {
+        return "HelloWorld"; //HelloWorld is how this module will be referred to from React Native
+    }
+
+    @ReactMethod
+    public void sayHello(Promise promise) { //this method will be called from JS by React Native
+        promise.resolve("Hello from Android");
+    }
+}
